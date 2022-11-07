fad308cdb0549b67d3ebce714f2741094ebccd7e
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-07 21:23:56 +0100

    finito (responsive)

--- js/script.js
+++ js/script.js
@@ -1,52 +1,219 @@
-/*
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-*/
-
 const eleContainer = document.querySelector('.container');
-
 const objMembers = [
-    membro1 = {
+    {
         name: 'Wayne Barnett',
         job: 'Founder & CEO',
         picture: 'img/wayne-barnett-founder-ceo.jpg'
     },
-    membro2 = {
+    {
         name: 'Angela Caroll',
         job: 'Chief Editor',
         picture: 'img/angela-caroll-chief-editor.jpg'
     },
-    membro3 = {
+    {
         name: 'Walter Gordon',
         job: 'Office Manager',
         picture: 'img/walter-gordon-office-manager.jpg'
     },
-    membro4 = {
+    {
         name: 'Angela Lopez',
         job: 'Media Manager',
         picture: 'img/angela-lopez-social-media-manager.jpg'
     },
-    membro5 = {
+    {
         name: 'Scott Estrada',
         job: 'Developer',
         picture: 'img/scott-estrada-developer.jpg'
     },
-    membro6 = {
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        picture: 'img/barbara-ramos-graphic-designer.jpg'
+    }, {
+        name: 'Angela Lopez',
+        job: 'Media Manager',
+        picture: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        picture: 'img/scott-estrada-developer.jpg'
+    },
+    {
         name: 'Barbara Ramos',
         job: 'Graphic Designer',
         picture: 'img/barbara-ramos-graphic-designer.jpg'
     },
 ];
 
-// eleContainer.innerHTML += objMembers[1].name + ', ';
-// eleContainer.innerHTML += objMembers[1].job;
 
-const eleCard = document.querySelector('.cards');
-
+const eleCards = document.querySelector('.cards');
 for (let i = 0; i < objMembers.length; i++) {
 	const eleImg = document.createElement('img');
-	eleImg.src = objMembers[i].picture;
-	eleCard.append(eleImg);
+	const eleCard = document.createElement('div');
+	const eleP = document.createElement('p');
 
-    console.log(objMembers[i]);
+    eleCard.classList.add('card-container')
+	eleImg.src = objMembers[i].picture;
+    eleP.innerHTML += objMembers[i].name + ' ' + objMembers[i].job;
+
+	eleCard.append(eleImg);
+	eleCard.append(eleP);
+	eleCards.append(eleCard);
 }
+
